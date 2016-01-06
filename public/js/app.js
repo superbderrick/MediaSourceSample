7e905851f03c627a756507d98b26e9f2b5e71016
Derrick <kang.derrick@gmail.com>
2016-01-06 14:16:09 +0900

    update files

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,38 @@
+var player = require("/player");
 var app = function() {};
 
 app.testURL = null;
 
-app.uri = null;
 
 
 
-app.onContentChange = function () 
-{
-
+app.onContentChange = function ()  {
   var mpd = document.getElementById('contentsList').value;
   app.testType = mpd ;
   
 }
+
+app.loadStream = function () {
+    player.init();
+}
+
+
+var player = function(video) 
+{
+  player.mediaSource = null;
+  player.sourceBuffer = null;
+  player.mimeCodec = null;
+  player.video = null;
+
+  player.segmentLength = 0;
+  player.segmentDuration = 0;
+    player.bytesFetched = 0;
+    player.requestedSegments = [];
+    player.totalSegments = 5;
+
+    player.init = function  () {
+        console.log('test');
+    }
+
+}
+
