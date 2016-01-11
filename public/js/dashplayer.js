162b89f883b67380c852f8eeb125e970d87cffa1
derrick <kang.derrick@gmail.com>
2016-01-11 11:42:13 +0900

    update dash.js

--- public/js/dashplayer.js
+++ public/js/dashplayer.js
@@ -29,18 +29,21 @@ dashPlayer.bufferUpdated = false;
 dashPlayer.Index = 0;
 dashPlayer.segLength = 0;
 
-
-
-
-
+/**
+ * [init setdatas url , video element to play  ]
+ * @param  {[string]} url   [contents url]
+ * @param  {[video element]} video [video element from html] 
+ */
 dashPlayer.init = function(url, video) {
     dashPlayer.url = url;
     dashPlayer.video = video;
 }
 
+/**
+ * [play startPlay! ]
+ */
 dashPlayer.play = function() {
 		dashPlayer.parseMPD();
-    
 }
 
 dashPlayer.setVideo = function() {
@@ -49,10 +52,12 @@ dashPlayer.setVideo = function() {
   dashPlayer.mediaSource.addEventListener('sourceopen', dashPlayer.sourceOpen);
 }
 
+/**
+ * [parseMPD parseMpddata to dash play]
+ */
 dashPlayer.parseMPD = function() {
 	dashPlayer.getData(dashPlayer.url);
 }
-
 
 dashPlayer.getData = function(url) {
 	var xhr = new XMLHttpRequest(); // Set up xhr 
@@ -65,10 +70,9 @@ dashPlayer.getData = function(url) {
 	    var parser = new DOMParser(); //  create a parser object 
 
 	    // Create an xml document from the .mpd file for searching
-	    xmlData = parser.parseFromString(tempoutput, "text/xml", 0);
+	    var xmlData = parser.parseFromString(tempoutput, "text/xml", 0);
 
-
-	    // Get and display the parameters of the .mpd file
+	    // Get  the parameters of the .mpd file
 	    dashPlayer.getFileType(xmlData);
 
       dashPlayer.setVideo();
@@ -91,7 +95,6 @@ dashPlayer.getFileType = function( data) {
 	dashPlayer.Initialization = ini[0].getAttribute("range");
 	dashPlayer.segments = data.querySelectorAll("SegmentURL");
   dashPlayer.segmentDuration = segList[0].getAttribute("duration");
-
 	}
 	catch (error) {
 		console.log(error);
@@ -144,21 +147,38 @@ dashPlayer.timeToDownload = function(range) {
 }
 
 dashPlayer.updateFunct = function() {
-	dashPlayer.bufferUpdated = true;
-  dashPlayer.getStarted(dashPlayer.file);
-  dashPlayer.sourceBuffer.removeEventListener("update", dashPlayer.updateFunct);
+  console.log('dashPlayer.updateFunct');
 
-    // videoSource.removeEventListener("update", updateFunct);
+	dashPlayer.bufferUpdated = true;
+  dashPlayer.video.play();
+  //dashPlayer.getStarted(dashPlayer.file);
+  //dashPlayer.sourceBuffer.removeEventListener("update", dashPlayer.updateFunct);
+    
 }
 
 dashPlayer.getStarted = function(url) {
+  console.log('dashPlayer.getStarted');
   dashPlayer.playSegment(dashPlayer.segments[dashPlayer.Index].getAttribute("mediaRange").toString(), url);
 	dashPlayer.Index++;	
   dashPlayer.video.addEventListener("timeupdate", dashPlayer.fileChecks, false);
-
+  dashPlayer.video.addEventListener('canplay', dashPlayer.canPlayEvent);   
+  dashPlayer.video.addEventListener('error', dashPlayer.showErrorMessage);  
 }
 
+dashPlayer.canPlayEvent = function () {
+   console.log('canPlayEvent');
+   dashPlayer.video.play();
+}
+
+dashPlayer.showErrorMessage = function () {
+   console.log('showErrorMessage');
+}
+
+
 dashPlayer.playSegment = function(range , url) {
+  console.log('dashPlayer.playSegment');
+  console.log('dashPlayer.playSegment' + range);
+  console.log('dashPlayer.playSegment' + url);
 	   var xhr = new XMLHttpRequest();
       if (range || url) { // make sure we've got incoming params
         xhr.open('GET', url);
@@ -171,11 +191,8 @@ dashPlayer.playSegment = function(range , url) {
               //  Calculate when to get next segment based on time of current one
               //  // dashPlayer.segCheck = (dashPlayer.timeToDownload(range) * .8).toFixed(3); // use .8 as fudge factor        
                 dashPlayer.segCheck = (dashPlayer.timeToDownload(range) * .8).toFixed(3); // use .8 as fudge factor
-                dashPlayer.segLength.textContent = dashPlayer.segCheck;
               // Add received content to the buffer
               try {
-                console.log(dashPlayer.sourceBuffer);
-                console.log(new Uint8Array(xhr.response));
                 dashPlayer.sourceBuffer.appendBuffer(new Uint8Array(xhr.response));
               } catch (e) {
                 console.log('Exception while appending', e);
@@ -191,6 +208,7 @@ dashPlayer.playSegment = function(range , url) {
 }
 
 dashPlayer.fileChecks = function() {
+  console.log("dashPlayer.fileChecks");
 // If we're ok on the buffer, then continue
   if (dashPlayer.bufferUpdated == true) {
     if (dashPlayer.Index < dashPlayer.segments.length) {
