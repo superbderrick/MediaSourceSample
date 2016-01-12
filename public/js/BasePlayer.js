b4a14fd19a9b52fe3cc2bf63bec507672ef91f25
derrick <kang.derrick@gmail.com>
2016-01-12 16:42:02 +0900

    added comment for explation

--- public/js/BasePlayer.js
+++ public/js/BasePlayer.js
@@ -12,24 +12,21 @@ BasePlayer.init = function(url, video) {
 }
 
 BasePlayer.start = function() {
- if ('MediaSource' in window && MediaSource.isTypeSupported(BasePlayer.mimeCodec)) {
 
-      BasePlayer.mediaSource = new MediaSource; // step 1 create Mediasource Object. 
+ if ( MediaSource.isTypeSupported(BasePlayer.mimeCodec)) { // check support mime type.
 
-      BasePlayer.video.src = URL.createObjectURL(BasePlayer.mediaSource); // step 2 Attach it to a video element and Creates URLs for MediaSource objects. 
-      
-      BasePlayer.mediaSource.addEventListener('sourceopen', BasePlayer.sourceOpen); // step3 Register source openevent. 
+      BasePlayer.mediaSource = new MediaSource; // step 1 Create Mediasource Object. 
+
+      BasePlayer.video.src = URL.createObjectURL(BasePlayer.mediaSource); 
+      // step 2 Attach it to a video element and Create a virtual URL using createObjectURL with the MediaSource object as the source.      
+      BasePlayer.mediaSource.addEventListener('sourceopen', BasePlayer.sourceOpen); //  Register source openevent. 
     } else {
       console.error('Unsupported MIME type or codec: ', BasePlayer.mimeCodec);
     }	
 }
 
-/**
- * [sourceOpen ]
- * @return {[type]} [description]
- */
 BasePlayer.sourceOpen = function() {
-	BasePlayer.sourceBuffer = BasePlayer.mediaSource.addSourceBuffer(BasePlayer.mimeCodec);	// step4 
+	BasePlayer.sourceBuffer = BasePlayer.mediaSource.addSourceBuffer(BasePlayer.mimeCodec);	// step4 Create a SourceBuffer using addSourceBuffer, with the mime type of the video you're adding. 
 	BasePlayer.fetchSegment(BasePlayer.url , BasePlayer.updateEnd);
 }
 
@@ -38,19 +35,19 @@ BasePlayer.fetchSegment = function(url , callback) {
     xhr.open('get', url);
     xhr.responseType = 'arraybuffer';
     xhr.onload = function () {
+      // The fetch succeeded.
       callback(xhr.response);
     };
     xhr.send();
 }
 
 BasePlayer.updateEnd = function(chunk) {
-  BasePlayer.sourceBuffer.appendBuffer(chunk);
-	BasePlayer.sourceBuffer.addEventListener('updateend' , BasePlayer.checkStatus);
+  BasePlayer.sourceBuffer.appendBuffer(chunk); //step5 Appends the specified media segment to the SourceBuffer.
+	BasePlayer.sourceBuffer.addEventListener('updateend' , BasePlayer.play);
 }
 
-BasePlayer.checkStatus = function() {
-  console.log('checkstatus');
-	BasePlayer.mediaSource.endOfStream();
+BasePlayer.play = function() {
+  BasePlayer.mediaSource.endOfStream();
   video.play();
 }
 
